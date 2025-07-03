c2ba7c7530356a093643a3afd06d81b09fd82706
devle <dltjgus0709@gmail.com>
2025-07-04 01:50:33 +0900

    feat: Add token swap API
    - using EthereumTokenExchanger class

--- back/app/main.py
+++ back/app/main.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
 
+from app.model.swap_info import SwapRequest
+from app.token.eth_token_exchanger import EthereumTokenExchanger
+from app.token.token_exchanger import TokenExchanger
+
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.post("/swap")
+async def swap_token(swap_request: SwapRequest):
+    token_exchanger: TokenExchanger = EthereumTokenExchanger()
+    return token_exchanger.swap_token(swap_request)
