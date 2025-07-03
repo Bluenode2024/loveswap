681aa569515ed70e4911f6a37813f667bb9c1597
devle <dltjgus0709@gmail.com>
2025-07-04 01:56:27 +0900

    feat: Add UserInfo struct definition for user info data token

---
+++ contract/contracts/UserInfo.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+struct UserInfo {
+    string instagramId;
+    string major;
+    uint8 majorType;
+    uint8 mbtiIeType;
+    uint8 mbtiNtsfType;
+    uint8 mbtiPjType;
+    uint8 appearanceType;
+    uint8 hobby;
+    uint8 debateStance;
+}
